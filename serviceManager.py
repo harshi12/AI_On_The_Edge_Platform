bdd17b7cc33a11b3d664905aed3c3753e0fe6c4e
harshita agrawal <harshita.agrawal@students.iiit.ac.in>
2019-03-19 15:01:46 +0530

    service manager with live batch functionality - yet to be tested

--- serviceManager.py
+++ serviceManager.py
@@ -9,10 +9,13 @@ RMQ = RabbitMQ()
 model_port_map = {}
 model_IPS = []
 model_port_index = {}
-
+port_list = []
+live_threads = {}
+thread_stop_flag = {}
 
 class serviceManager:
     def __init__(self):
+        self.lock = threading.Lock()
         t1 = Thread(target = self.receive_AD_input, args = ('', "AD_SM"))
         t2 = Thread(target = self.receive_scheduler_input, args = ('', "Scheduler_SM"))
         t3 = Thread(target = self.receive_DM_input, args = ('', "Docker_SM"))
@@ -24,7 +27,6 @@ class serviceManager:
         # print("*****************",body)
         data = json.loads(body)
         # print(data)
-        # print("Data received successfully")
         # print("################################")
         # print("parsed")
         return data
@@ -35,19 +37,25 @@ class serviceManager:
         body = body.decode()
         data = json.loads(body)
         data = ast.literal_eval(data)
-        print("Data Before:",data)
-        print("Data Type: ",type(data))
+        print("Data received successfully")
+        # print("Data Before:",data)
+        # print("Data Type: ",type(data))
         return data
 
     def receive_DM_input(self,exchange, key):
+        print("In docker receiver queue")
         RMQ.receive(self.process_DM_Input, exchange, key)
 
     def process_DM_Input(self, ch, method, properties, body):
         global model_port_map
         data = json.loads(body)
-        ack_response = data["ack_response"]
-        for key in ack_response.keys():
-            model_port_map[key] = ack_response[key]
+        data = ast.literal_eval(data)
+        print("Docker data",data)
+        global port_list
+        ack_response = data["Ack"]
+        print("Docker Manager Response: ", ack_response)
+        model_name = ack_response["Model"]
+        model_port_map[model_name] = ack_response["IP:Port"]
 
     def sendtoScheduler(self, ch, method, properties, body):
         print("data from AD %r"%body)
@@ -63,62 +71,156 @@ class serviceManager:
             print("here")
             RMQ.receive(self.process_scheduler_input, exchange, key)
 
+    def get_thread_id(self): 
+        # returns id of the respective thread 
+        if hasattr(self, '_thread_id'): 
+            return self._thread_id 
+        for id, thread in threading._active.items(): 
+            if thread is self: 
+                return id
+
+    def inference_LiveData(self, model_name):
+        thread_id = self.get_thread_id()
+        while True:
+            if thread_stop_flag[thread_id]:
+                break
+
+            body = RMQ.receive_nonblock("", "Model1_Input")
+            if body is None:
+                continue
+
+            body = body.decode()
+            data = json.loads(body)
+            data = ast.literal_eval(data)
+            print("Data received successfully")
+            print(data)
+            data_rows = data["data"]
+
+            for i in range(len(data_rows)):
+                test_data = {}
+                test_data["signature_name"] = "model"
+                test_data["instances"] = [data_rows[i]]
+                test_data = json.dumps(test_data)
+
+                service_ip_port = ""
+
+                print("Acquiring Lock in thread")
+                self.lock.acquire()
+                try:
+                    i = model_port_index[model_name]
+                    service_ip_port = model_ip_ports[i]
+                    i = (i+1) % len(model_ip_ports)
+                    model_port_index[model_name] = i
+                finally:
+                    self.lock.release()
+                    print("Lock released in thread")
+
+                print("Prediction model sent to", service_ip_port)
+
+                headers = {"content-type": "application/json"}
+                request_string = str('http://'+service_ip_port+'/v1/models/'+model_name+':predict')
+                print("Request String", request_string)
+                json_response = requests.post(request_string, data=final_batch_test_data, headers=headers)
+                print("After making REST call")
+                # i = (i+1) % len(model_ip_ports)
+                # self.lock.acquire()
+                # try:
+                #     model_port_index[model_name] = i
+                #     print("IP:Port index updated in thread!")
+                # finally:
+                #     self.lock.release()
+
+                print (json_response.text)
+
+                RMQ.send("", "Model1_Output", json_response.text)
+
     def process_scheduler_input(self, ch, method, properties, body):
         print("1")
         data = json.loads(body)
         print(body)
+        model_name = data["model"]
+
         if data["request_type"] == 'deploy':
-            message = {"Request_type" : "Deploy", "Link" : data["model"], "No_Hosts" : 1, "IPs" : ["192.168.43.137"]}
+            message = {"Request_type" : "Deploy", "Link" : data["model"], "No_Hosts" : 1, "IPs" : ["192.168.43.103"]}
             global model_IPS
-            model_port_index[data["model"]] = 0
+            model_port_index[model_name] = 0
             model_IPS = message["IPs"]
             message = json.dumps(message)   
             RMQ.send("","SM_Docker", message)
 
         elif data["request_type"] == 'kill':
-            pass
-
+            message = {"Request_type" : "Kill", "Link" : data["model"], "No_Hosts" : 1, "IPs" : ["192.168.43.103"]}
+            thread_id = live_threads[model_name]
+            thread_stop_flag[thread_id] = 1
+            
         elif data["request_type"]    == "inference_batch":
             print("Inference Batch Request")
             final_batch_test_data = {}
             batch_test_data = self.receive_InputStream_input("", "Model1_Input")
 
-            final_batch_test_data['signature_name'] = 'model'
-            final_batch_test_data['instances'] = batch_test_data['data']
+            final_batch_test_data["signature_name"] = "model"
+            final_batch_test_data["instances"] = batch_test_data['data']
 
             print("Testing Data:")
             print(final_batch_test_data)
 
-            model_name = data["model"]
-            model_ports = model_port_map[model_name]
-            
-            i = model_port_index[model_name]
-            i = i%len(model_ports)
+            final_batch_test_data = json.dumps(final_batch_test_data)
 
-            print("Prediction model sent to", model_IPS[i],":",model_ports[i])
+            # model_name = data["model"]
+            model_ip_ports = model_port_map[model_name]
+            
+            service_ip_port = ""
+                
+            print("Acquiring Lock in main")
+            self.lock.acquire()
+            try:
+                i = model_port_index[model_name]
+                service_ip_port = model_ip_ports[i]
+                i = (i+1) % len(model_ip_ports)
+                model_port_index[model_name] = i
+            finally:
+                self.lock.release()
+                print("Lock released in main")
+
+            print("Prediction model sent to", service_ip_port)
+
+            # print("Prediction model sent to", model_ip_ports[i])
 
             headers = {"content-type": "application/json"}
-            json_response = requests.post('http://'+model_IPS[i]+':'+model_ports[i]+'/v1/models/'+model_name+':predict', data=final_batch_test_data, headers=headers)
-            i += 1
+            request_string = str('http://'+service_ip_port+'/v1/models/'+model_name+':predict')
+            print("Request String", request_string)
+            json_response = requests.post(request_string, data=final_batch_test_data, headers=headers)
+            print("After making REST call")
+            # print("Acquiring Lock in main")
+            # i += 1
+            # self.lock.acquire()
+            # try:
+            #     model_port_index[model_name] = i
+            #     print("IP:Port index updated in main!")
+            # finally:
+            #     self.lock.release()
+            #     print("Lock released in main")
+            # model_port_index[model_name] = i
             print (json_response.text)
 
             RMQ.send("", "Model1_Output", json_response.text)
 
         elif data["request_type"] == "inference_live":
-            pass
+            global thread_stop_flag
+            global live_threads
 
+            print("In inference live")
+            final_batch_test_data = {}
+            # model_name = data["model"]
+
+            t5 = Thread(target = self.inference_LiveData, args = (model_name))
+            t5.start()
+            thread_stop_flag[t5] = 0 #0 - thread is running, 1 - kill the thread
+            live_threads[model_name] = t5
+            
         return
 
-# def myfun(self, ch, method, properties, body):
-#     print("Received Data: %r"%body)
 
 if __name__ == '__main__':
     obj = serviceManager()
-    # obj
-#     message_ravi = {"Request_type" : "Deploy", "Link" : "This is my Link", "No_Hosts" : 1, "IPs" : ["192.168.43.137"]}
-#     message = {"request_type": "serve_model", "model": "1d_jdNRI7Ak-_CSPo6wYUUt0fj0_yKqjb"}
-#     message = json.dumps(str(message))
-#     RMQ.send("","SM_Scheduler", message)
-
-#     RMQ.receive(myfun, "", "Scheduler_SM")
 
