d401d66e7a238f1b02b389aa5899180f9cc0b242
Ravi Jakhania <ravi.jakhania@students.iiit.ac.in>
2019-04-12 23:59:21 +0530

    Functions working properly. Socket programing part is remaining.

--- API/distance_sensor.py
+++ API/distance_sensor.py
@@ -5,6 +5,8 @@ import random
 import time
 import datetime
 import _thread
+import threading 
+from timer import *
 
 class DistanceSensor(Sensor):
     def __init__(self, rate = 1, dest_IP = "127.0.0.1", dest_port = 4444):
@@ -15,25 +17,23 @@ class DistanceSensor(Sensor):
     # use sockets
     # create thread to send simulated input
     def send_simulated_input(self):
-        x = 0
-        while(x < 5):
-            Time = 
-            population = [0,1]
-            weights = [0.1, 0.9]
-            Number_Selected = choices(population, weights)
-            Generated_Distance = None
-            if(Number_Selected[0]):
-                Generated_Distance = random.randint(201,1001)
-            else:
-                Generated_Distance = random.randint(0,201)
-            print ("Time: ",datetime.datetime.now(), "---> Generated_Distance: ",Generated_Distance)
-            time.sleep(1)
-            #-------------- Send Generated_Distance using socket ------------- #
+        population = [0,1]
+        weights = [0.1, 0.9]
+        Number_Selected = choices(population, weights)
+        Generated_Distance = None
+        if(Number_Selected[0]):
+            Generated_Distance = random.randint(201,1001)
+        else:
+            Generated_Distance = random.randint(0,201)
+        print ("Time: ",datetime.datetime.now(), "---> Generated_Distance: ",Generated_Distance)
+        #-------------- Send Generated_Distance using socket ------------- #
 
     # receive display alarm service's output using sockets
     def receive_alarm_output(self):
-        print ("Time: ",datetime.datetime.now(),"Receive Call")
+        pass
+        # print ("Time: ",datetime.datetime.now(),"Receive Call")
         #-------------- Receive output using socket ------------- #
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print ("Usage: distance_sensor.py <sensor_manager_IP> <sensor_manager_port>")
@@ -41,19 +41,13 @@ if __name__ == "__main__":
 
     distance_sensor = DistanceSensor(dest_IP = sys.argv[1], dest_port = sys.argv[2])
 
-    print ("Starting send_simulated_input thread and receive_alarm_output Function")
-    try:
-        _thread.start_new_thread(distance_sensor.send_simulated_input,())
-    except:
-        print ("Error: unable to start thread")
-        sys.exit(1)
-    print ("send_simulated_input thread started")
+    # print ("Starting send_simulated_input thread and receive_alarm_output Function")
+    timer = RepeatedTimer(0,"DistanceSensor",distance_sensor.send_simulated_input)
+    # print ("send_simulated_input thread started")
 
-    print ("Starting receive_alarm_output function")
+    # print ("Starting receive_alarm_output function")
     distance_sensor.receive_alarm_output()
-    print ("Receive_alarm_output function started")
+    # print ("Receive_alarm_output function started")
 
-    while 1:
-        pass
-
-
+    # timer.stop()
+    # Remove after completing receive_alarm_output function
--- API/flower_analysis_sensor.py
+++ API/flower_analysis_sensor.py
@@ -1,20 +1,32 @@
 import sys
 from sensor import *
+import time
+import datetime
+import numpy as np
+import pandas as pd
+from timer import *
 
 class FlowerAnalysisSensor(Sensor):
     def __init__(self, rate = 0.5, dest_IP = "127.0.0.1", dest_port = 4444):
         description = "This is an iris-flower classification sensor."
         Sensor.__init__(self, description, "one-way", rate, dest_IP, dest_port)
 
+        self.dataset = pd.read_csv("./Iris.csv").values
+        self.Length = self.dataset.shape[0]
+        self.Index = 0
+
     # sends simulated input to the Sensor Manager in the prescribed rate
     # use sockets
-    def send_simulated_input():
-
+    def send_simulated_input(self):
+        print ("Time: ",datetime.datetime.now(), "---> Data: ",self.dataset[self.Index][:-1])
+        self.Index = (self.Index + 1) % self.Length
+        # ----------- send data using socket programming ----------- #
 
 if __name__ == "__main__":
-    if sys.argc < 2:
+    if len(sys.argv) < 2:
         print ("Usage: flower_analysis_sensor.py <sensor_manager_IP> <sensor_manager_port>")
         sys.exit(1)
 
     flower_analysis_sensor = FlowerAnalysisSensor(dest_IP = sys.argv[1], dest_port = sys.argv[2])
-    flower_analysis_sensor.send_simulated_input()
+    timer = RepeatedTimer(0,"FlowerAnalysisSensor",flower_analysis_sensor.send_simulated_input)
+    # flower_analysis_sensor.send_simulated_input()
