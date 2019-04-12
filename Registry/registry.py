4c181820da4a52937936db8cace93db0d8c51fa6
Pranjali Ingole <pranjali.ingole@students.iiit.ac.in>
2019-04-12 18:24:59 +0530

    Registry class implementation

--- Registry/registry.py
+++ Registry/registry.py
@@ -2,75 +2,83 @@ from queue_req_resp import RabbitMQ
 import json
 import threading
 
-# Global data structures
-Storage_info = {}
-Service_inst_info = {}
-App_inst_info = {}
 
-def Read_DS(DS_Name):
-    pass
+class Registry:
 
-def Write_DS(DS_Name, DS_Obj):
+    def __init__(self):
+        Storage_info = {}
+        Service_inst_info = {}
+        App_inst_info = {}
 
-    if(DS_Name=='Storage_info'):
-        for i in range(len(DS_Obj)):
-            #Record is a Dict
-            Record = DS_Obj[i]
-            App_Id = Record['App_id']
+    def Read_DS(self, DS_Name):
+        pass
 
-            Storage_info[App_Id] = {}
+    def Write_DS(self, DS_Name, DS_Obj):
 
-            Model_Link = Record['Model_Link']
-            App_Link = Record['App_Link']
-            Config_Link = Record['Config_Link']
+        if(DS_Name=='Storage_info'):
+            for i in range(len(DS_Obj)):
+                #Record is a Dict
+                Record = DS_Obj[i]
+                App_Id = Record['App_id']
 
-            Storage_info[App_Id]['Model_Link'] = Model_Link
-            Storage_info[App_Id]['App_Link'] = App_Link
-            Storage_info[App_Id]['Config_Link'] = Config_Link
+                self.Storage_info[App_Id] = {}
 
-    elif(DS_Name=='Service_inst_info'):
-        for i in range(len(DS_Obj)):
-            #Record if Dict
-            Record = DS_Obj[i]
-            Model_Id = Record['Model_id']
+                Model_Link = Record['Model_Link']
+                App_Link = Record['App_Link']
+                Service_Link = Record['Service_Link']
+                Config_Link = Record['Config_Link']
 
-            Service_inst_info[Model_Id] = []
+                self.Storage_info[App_Id]['Model_Link'] = Model_Link
+                self.Storage_info[App_Id]['App_Link'] = App_Link
+                self.Storage_info[App_Id]['Service_Link'] = Service_Link
+                self.Storage_info[App_Id]['Config_Link'] = Config_Link
 
-            for j in range(len(DS_Obj[i]['Hosts'])):
-                Hosts_List = DS_Obj[i]['Hosts']
-                Host_IP = Hosts_List[j][0]
-                Host_Port = Hosts_List[j][1]
-                Model_Status = Hosts_List[j][2]
+        elif(DS_Name=='Service_inst_info'):
+            for i in range(len(DS_Obj)):
+                #Record if Dict
+                Record = DS_Obj[i]
+                Model_Id = Record['Model_id']
 
-                Model_Inst = [Host_IP, Host_Port, Model_Status]
-                Service_inst_info[Model_Id].append(Model_Inst)
+                self.Service_inst_info[Model_Id] = []
 
-    elif(DS_Name=='App_inst_info'):
-        for i in range(len(DS_Obj)):
-             #Record if Dict
-             Record = DS_Obj[i]
-             App_Id = Record['App_id']
+                for j in range(len(DS_Obj[i]['Hosts'])):
+                    Hosts_List = DS_Obj[i]['Hosts']
+                    Host_IP = Hosts_List[j][0]
+                    Host_Port = Hosts_List[j][1]
+                    Model_Status = Hosts_List[j][2]
 
-             App_inst_info[App_Id] = []
+                    Model_Inst = [Host_IP, Host_Port, Model_Status]
+                    self.Service_inst_info[Model_Id].append(Model_Inst)
 
-             for j in range(len(DS_Obj[i]['Hosts'])):
-                 Hosts_List = DS_Obj[i]['Hosts']
-                 Host_IP = Hosts_List[j][0]
-                 Host_Port = Hosts_List[j][1]
-                 App_Status = Hosts_List[j][2]
+        elif(DS_Name=='App_inst_info'):
+            for i in range(len(DS_Obj)):
+                 #Record if Dict
+                 Record = DS_Obj[i]
+                 App_Id = Record['App_id']
 
-                 App_Inst = [Host_IP, Host_Port, App_Status]
-                 App_inst_info[App_Id].append(App_Inst)
-    else :
-        print("\nInvalid Data Structure Name\n")
+                 self.App_inst_info[App_Id] = []
 
-def Print_DS():
-    print("\nStorage_info: ", Storage_info)
-    print("\nService_inst_info: ", Service_inst_info)
-    print("\nApp_inst_info: ", App_inst_info)
+                 for j in range(len(DS_Obj[i]['Hosts'])):
+                     Hosts_List = DS_Obj[i]['Hosts']
+                     Host_IP = Hosts_List[j][0]
+                     Host_Port = Hosts_List[j][1]
+                     App_Status = Hosts_List[j][2]
+
+                     App_Inst = [Host_IP, Host_Port, App_Status]
+                     self.App_inst_info[App_Id].append(App_Inst)
+        else :
+            print("\nInvalid Data Structure Name\n")
+
+    def Print_DS(self):
+        print("\nStorage_info: ", self.Storage_info)
+        print("\nService_inst_info: ", self.Service_inst_info)
+        print("\nApp_inst_info: ", self.App_inst_info)
 
 # Read JSON from common queue , parse it and call Update_DS/Read_DS function
 def callback(ch, method, properties, body):
+
+    Registry_obj = Registry()
+
     global port
     print ("Receiving from Common queue")
     #body = body.decode("utf-8")
@@ -79,20 +87,20 @@ def callback(ch, method, properties, body):
     #Receiving_Message = json.loads(body).replace('\'','\"')
     Receiving_Message = json.loads(body)
     print("\nReceiving_Message: ", Receiving_Message)
-    
+
     Request_type = Receiving_Message['Request_Type']
     DS_Name = Receiving_Message['DS_Name']
 
     if(Request_type=='Read'):
-        DS_Value = Read_DS(DS_Name)
+        DS_Value = Registry_obj.Read_DS(DS_Name)
         #create JSON and send on temp queue
 
     if(Request_type=='Write'):
         DS_Obj = Receiving_Message['Value']
-        Write_DS(DS_Name, DS_Obj)
+        Registry_obj.Write_DS(DS_Name, DS_Obj)
 
     print("\nUpdated Data Structures\n ")
-    Print_DS()
+    Registry_obj.Print_DS()
 
 def Recieve_from_SM():
     msg_obj2 = RabbitMQ()
