3237b5d3a5b816dd4ef3903ab68748e4ecbd5e13
Bhavi Dhingra <bhavi.dhingra@students.iiit.ac.in>
2019-04-12 16:48:38 +0530

    modifying distance_alarm and sonar sensor APIs

--- API/distance_alarm_service.py
+++ API/distance_alarm_service.py
@@ -1,5 +1,12 @@
 class DistanceAlarmService(Service):
-
+    def __init__(self, emergency_start, emergency_end, critical_start, critical_end,
+                 all_fine_start, all_fine_end):
+        self.em_start = emergency_start
+        self.em_end = emergency_end
+        self.cr_start = critical_start
+        self.cr_end = critical_end
+        self.fine_start = all_fine_start
+        self.fine_end = all_fine_end
 
 if __name__ == "__main__":
     
--- API/sonar_sensor.py
+++ API/sonar_sensor.py
@@ -5,6 +5,10 @@ class SonarSensor(Sensor):
         description = "This is a Naval mine sensor taking in Sonar Data."
         Sensor.__init__(self, description, "one-way", rate, dest_IP, dest_port)
 
+    # sends simulated input to the Sensor Manager in the prescribed rate
+    # use sockets
+    # create thread to send simulated input
+    def send_simulated_input():
 
 if __name__ == "__main__":
     if sys.argc < 2:
@@ -12,3 +16,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     sonar_sensor = SonarSensor(dest_IP = sys.argv[1], dest_port = sys.argv[2])
+    sonar_sensor.send_simulated_input()
