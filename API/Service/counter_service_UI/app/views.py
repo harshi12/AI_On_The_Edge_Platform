7c6a49f3ac4a14483ba5e90d595cbf42bd74f883
Hitesh <hiteshkaushik545@gmail.com>
2019-04-17 04:45:17 +0530

    Added functionality in counter and helper service to send their outputs on platform output stream

--- API/Service/counter_service_UI/app/views.py
+++ API/Service/counter_service_UI/app/views.py
@@ -1,30 +1,38 @@
+import sys
+sys.path.insert (0, '../../')
+sys.path.insert (0, '../../../')
+from RabbitMQ.message_queue import *
 from app import app
-import pika
 import json
-import pandas as pd
 from flask import Flask,render_template,request,redirect
 from threading import Thread
-import numpy as np
 from googleapiclient.discovery import build
 from oauth2client import client, tools, file
 from googleapiclient.http import MediaFileUpload
-from httplib2 import Http
-import time
 
 SCOPES = 'https://www.googleapis.com/auth/drive'
-
+RMQ = RabbitMQ()
+count = 0
 @app.route('/')
 def firstpage():
+	# TODO : Insert proper queuename in next line
+    t1 = Thread(target = receiveInput, args = ('', "temp")) #thread that will monitor HM_SM Queue	
+    t1.start()
     return render_template('p.html',title='Counter Service UI')
+
+def receiveInput(exchange, key):
+	RMQ.receive(callback, exchange, key)
+
+def callback(ch, method, properties, body):
+	global count
+	if not isinstance(body, str):
+		body = body.decode()
+	count = int(body)
 
 @app.route('/counter')
 def getCount():
-    '''
-        When called, this function will receive data from some stream and send it back to the caller
-		# TODO : Implement the input stream listener        
-    '''
-    getCount.count += 1
-    data = {"count" : getCount.count}
-    data = json.dumps(data)
-    return data
-getCount.count = 0
+	''' When called, this function will receive data from some stream and send it back to the caller '''
+	global count
+	data = {"count" : count}
+	data = json.dumps(data)
+	return data
