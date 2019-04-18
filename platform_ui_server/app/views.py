a862f7ffe1a6c47770c4c20c70cbf74b8d8e8f83
Sukku1 <sai.sukrutha@students.iiit.ac.in>
2019-04-18 22:24:55 +0530

    UIchanges-get app ui ips

--- platform_ui_server/app/views.py
+++ platform_ui_server/app/views.py
@@ -21,6 +21,24 @@ from queue_req_resp import RabbitMQ
 
 from werkzeug.utils import secure_filename
 
+from threading import Thread
+
+RMQ=RabbitMQ()
+
+def ReceivefromSM(self, exchange, key):
+    print("Listening......")
+    RMQ.receive(self.processInput, exchange, key)
+
+def processInput( ch, method, properties, body):
+    data = json.loads(body)
+    app_link = data['App_Link']
+    ip = data['IP']
+    port = data['Port']
+    ui_server=ip+":"+port
+
+t=Thread(target=ReceivefromSM,args = ('', "SM_Flask"))
+
+
 BATCH_COUNT = 10
 
 #APP_UPLOAD_FOLDER = '/home/bhavidhingra/google-drive-iiith/Semester_#2/CSE563_Internals_of_Application_Servers/Hackathon/self_after_3/platform_ui_server1/Downloads/Applications/'
@@ -31,60 +49,6 @@ GW_UPLOAD_FOLDER = '/home/bhavidhingra/google-drive-iiith/Semester_#2/CSE563_Int
 #app = Flask(__name__)
 app.config['APP_UPLOAD_FOLDER'] = APP_UPLOAD_FOLDER
 app.config['GW_UPLOAD_FOLDER'] = GW_UPLOAD_FOLDER
-
-class RabbitMQ:
-    def __init__(self):
-    	self.server_IP = "192.168.43.135"
-    	self.server_Port = 5672
-    	self.credentials = pika.PlainCredentials("harshita","123")
-    	self.create_queue("", "AD_SM")
-    	self.create_ServiceQueues("SM","Docker")
-    	self.create_ServiceQueues("SM", "Scheduler")
-
-    def create_queue(self, exchange_name, queue_name):
-    	channel, conn = self.create_connection()
-    	# channel.exchange_declare(exchange='', exchange_type='direct')
-    	channel.queue_declare(queue = queue_name, durable = True)
-    	# channel.queue_bind(exchange=exchange_name, queue=queue_name)
-    	conn.close()
-
-    def create_ServiceQueues(self,Module1, Module2):
-    	self.create_queue("", str(Module1+"_"+Module2))
-    	self.create_queue("", str(Module2+"_"+Module1))
-
-    def create_connection(self):
-    	connection = pika.BlockingConnection(pika.ConnectionParameters(self.server_IP, self.server_Port, '/', self.credentials))
-    	channel = connection.channel()
-    	return channel, connection
-
-    def send(self,exchange_name, queue_name, message):
-    	channel, conn = self.create_connection()
-    	self.create_queue(exchange_name, queue_name)
-    	channel.basic_publish(exchange='', routing_key=queue_name, body=message)
-    	# print(" [x] Sent",message)
-    	conn.close()
-
-    messages=[]
-
-    def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
-        #process(body)
-        messages.append(body)
-
-    def receive(self, callback, exchange_name, queue_name):
-        channel, conn = self.create_connection()
-        self.create_queue(exchange_name, queue_name)
-        channel.basic_consume(callback, queue = queue_name, no_ack = True)
-        print(' [*] Waiting for messages. To exit press CTRL+C')
-        channel.start_consuming()
-
-    def receive_nonblock(self, exchange_name, queue_name):
-        channel, conn = self.create_connection()
-        self.create_queue(exchange_name, queue_name)
-        method_frame, header_frame, body = channel.basic_get(queue_name, True)
-        # body = channel.basic_get(queue_name, True) #callback, queue = queue_name, no_ack = True)
-        # print("In queue:", type(body))
-        return body
 
 # print(os.getcwd())
 # # os.
